474eabdd7c7fc68e9f98a4dc368138fcf1419715
vikaspulluri <vikasiiitn@gmail.com>
2019-02-16 02:03:58 +0530

    updating api url

--- docs/api_data.js
+++ docs/api_data.js
@@ -1,33 +1,5 @@
 define({ "api": [
   {
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "optional": false,
-            "field": "varname1",
-            "description": "<p>No type.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "varname2",
-            "description": "<p>With type.</p>"
-          }
-        ]
-      }
-    },
-    "type": "",
-    "url": "",
-    "version": "0.0.0",
-    "filename": "./docs/main.js",
-    "group": "E__Vikas_workspace_todolist_backend_docs_main_js",
-    "groupTitle": "E__Vikas_workspace_todolist_backend_docs_main_js",
-    "name": ""
-  },
-  {
     "version": "1.0.0",
     "parameter": {
       "fields": {
